c9296bd71cc5fd52bc49c07249421904c8e5d5c4
André Sá de Mello <asmello.br@gmail.com>
2018-02-03 10:23:43 -0200

    allow move selection by coordinates

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
 #!/usr/bin/env python3
 
+import re
+
 from game import Game, IllegalActionException
 from ai import AI
+
+
+def human_move(game):
+    while True:
+        m = re.search("([a-zA-Z])(\d)$", input("Enter your move: "))
+        if m:
+            my_move = 9 * (ord(m[1].upper()) - ord('A')) + int(m[2])
+        else:
+            print("Unknown move... Try like A3.")
+            continue
+        try:
+            print(game.apply(my_move))
+            break
+        except IllegalActionException:
+            print("Illegal move... Try again.")
+
+
+def atop(action):
+    return chr(action // 9 + ord('A')) + str(action % 9 + 1)
 
 
 def main(args):
@@ -25,29 +46,17 @@ def main(args):
 
         first = input("Do you wish to play first? ")
         if first and first[0].lower() == 'y':
-            while True:
-                my_move = int(input("Enter your move: "))
-                try:
-                    print(game.apply(my_move))
-                    break
-                except IllegalActionException:
-                    print("Illegal move... Try again.")
+            human_move(game)
 
         while not game.over:
             ai_move = game.best_action
-            print("AI plays:", ai_move)
+            print("AI plays:", atop(ai_move))
             print(game.apply(ai_move))
 
             if game.over:
                 break
 
-            while True:
-                my_move = int(input("Enter your move: "))
-                try:
-                    print(game.apply(my_move))
-                    break
-                except IllegalActionException:
-                    print("Illegal move... Try again.")
+            human_move(game)
 
         print(f"Game over: {game.winner} wins!")
 
